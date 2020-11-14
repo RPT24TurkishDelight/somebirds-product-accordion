e2005d42be13a0c28df9139677d047cdd8a7c621
Gabriel Gutierrez <gabe.grez@gmail.com>
2020-11-13 23:19:49 -0800

    Request/send correct data from/to client

--- client/components/productMenu.jsx
+++ client/components/productMenu.jsx
@@ -1,8 +1,21 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import Accordian from './accordian.jsx';
+import axios from 'axios';
+
 
 const ProductMenu = (props) => {
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `/products/${props.modelName}`
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+  });
+
 
   return (
     <div className='menu'>
--- server.js
+++ server.js
@@ -2,17 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const port = 3002;
 const app = express();
-
-//put database in when ready to setup
+const helpers = require('./database/helpers.js')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/products/:shoeId', (req, res) => {
+app.get('/products/:shoeName', (req, res) => {
   console.log('inside GET api');
+  console.log(req.params.shoeName);
 
+  helpers.getShoeData(req.params.shoeName, (err, doc) => {
+    if (err) {
+      res.sendStatus(404);
+    }
+    console.log('////////server/////////', doc[0]);
+    res.send(doc[0]);
+  });
 })
 
 
