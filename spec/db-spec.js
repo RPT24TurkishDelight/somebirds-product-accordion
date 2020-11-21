fdac9e651466c04ac09a3b65195b6a21c2c804cb
Gabriel Gutierrez <gabe.grez@gmail.com>
2020-11-20 18:42:48 -0800

    Reorganize code to help with testing

--- spec/db-spec.js
+++ spec/db-spec.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect;
 const seed = require('../database/seed.js');
 const db = require('../database/index.js');
 const Shoe = require('../database/Product.js').shoe;
+const helpers = require('../database/helpers.js');
 
 
 describe('Creating a database', function() {
@@ -18,28 +19,36 @@ describe('Creating a database', function() {
   })
 
 
-  // it('should have a shoes collection ', function() {
-  //   mongoose.connection.db.listCollections((err, names) => {
-  //     if (err) {
-  //       console.log(err);
-  //     }
-  //     expect(names).to.include('shoes');
-  //   })
-  // });
 
-  it('should have correct data shape for generated data', function() {
-    let allStrings = fakeData[0].coreFeatures.every((feature) => {
-      return (typeof feature === 'string')
+  describe('Seed testing', () => {
+
+    it('should have correct data shape for generated data', function() {
+      let allStrings = fakeData[0].coreFeatures.every((feature) => {
+        return (typeof feature === 'string')
+      });
+      expect(fakeData[0].modelId).to.be.a('number');
+      expect(fakeData[0].name).to.be.a('string');
+      expect(allStrings).to.be.true;
+      expect(fakeData[0].description).to.be.a('string');
     });
-    expect(fakeData[0].modelId).to.be.a('number');
-    expect(fakeData[0].name).to.be.a('string');
-    expect(allStrings).to.be.true;
-    expect(fakeData[0].description).to.be.a('string');
+
+    it('should have at least 100 documents', function() {
+      Shoe.count({}, (err, count) => {
+        expect(count).to.equal(100);
+      });
+    });
   });
 
-  it('should have at least 100 documents', function() {
-    Shoe.count({}, (err, count) => {
-      expect(count).to.equal(100);
+  describe('API Endpoints', () => {
+
+    it('GET request to /shoes/:shoeName/summary returns correct data', () => {
+      helpers.getShoeData('mens-wool-runners', (err, doc) => {
+        if (err) {
+          throw err;
+        }
+        expect(doc[0].modelId).to.equal(1);
+        expect(doc[0].description).to.equal('Ethically made with ZQ Merino wool, our original sneaker is soft, moisture wicking, and ready for anything. ');
+      })
     });
   });
 
