c95fa747477454e0daf0fad1fb8c81a07f92babd
Gabriel Gutierrez <gabe.grez@gmail.com>
2020-11-23 21:32:47 -0800

    Commit before removing bundle from cache

--- client/components/Icon.jsx
+++ client/components/Icon.jsx
@@ -6,8 +6,8 @@ function Icon(props) {
     <div className='icon-text'>
       <svg
         className='highlight-icon'
-        height={props.height || 50}
-        width={props.width || 50}
+        height={props.height || 48}
+        width={props.width || 48}
         viewBox={props.viewBox}
         xmlns="http://www.w3.org/2000/svg"
         dangerouslySetInnerHTML={{__html: props.content}}
