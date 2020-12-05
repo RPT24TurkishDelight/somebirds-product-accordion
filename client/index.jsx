4f234772defe4011b6eed34066931ec30a015932
Gabriel Gutierrez <gabe.grez@gmail.com>
2020-12-05 13:12:07 -0800

    Change div tag name to render component

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ProductMenu from './components/productMenu.jsx';
 
-ReactDOM.render(<ProductMenu modelName={'mens-wool-runners'} modelId={1}/>, document.getElementById('summary'));
+ReactDOM.render(<ProductMenu modelName={'mens-wool-runners'} modelId={1}/>, document.getElementById('accordion-container'));
