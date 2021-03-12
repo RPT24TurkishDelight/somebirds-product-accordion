26b575007801e54c6e3d981afdc09933a8f9d94f
Aowei Chenqi <aoweichenqi@Aoweis-MacBook-Pro.local>
2021-03-11 20:22:21 -0800

    Added Caching

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const helpers = require('./database/psql.js');
 const port = 3002;
 const cors = require('cors');
 const axios = require('axios');
-
+const redis = require("redis");
+const redisPort = 6379
+const client = redis.createClient(redisPort);
 const app = express();
+
+client.on("error", (err) => {
+  console.log(err);
+});
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,12 +21,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/products/:shoeId/summary', (req, res) => {
-  helpers.getShoeData(req.params.shoeId, (err, doc) => {
-    if (err) {
-      res.sendStatus(404);
-    }
-    res.send(doc.rows[0]);
-  });
+  const id = req.params.shoeId;
+  try {
+    client.get(id, async (err, jobs) => {
+      if (err) throw err;
+
+      if (jobs) {
+        res.status(200).send(JSON.parse(jobs));
+      } else {
+        await helpers.getShoeData(id, (err, doc) => {
+          if (err) {
+            res.sendStatus(404);
+          }
+          client.setex(id, 600, JSON.stringify(doc.rows[0]));
+          res.status(200).send(doc.rows[0]);
+        });
+      }
+    })
+  } catch (err) {
+    res.status(404).send()
+  }
+  // helpers.getShoeData(id, (err, doc) => {
+  //   if (err) {
+  //     res.sendStatus(404);
+  //   }
+  //   res.status(200).send(doc.rows[0]);
+  // });
 });
 
 app.post('/products', (req, res) => {
